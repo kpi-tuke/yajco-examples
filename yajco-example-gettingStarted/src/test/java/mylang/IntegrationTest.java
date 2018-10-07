5df0c9dc6bbb5115f992cce9cf33a9760f406e8c
Bc. Michal Fecenko <michal.fecenko@student.tuke.sk>
2018-10-07 16:02:57 +0200

    added test to getting started example

---
+++ yajco-example-gettingStarted/src/test/java/mylang/IntegrationTest.java
@@ -0,0 +1,19 @@
+package mylang;
+
+import mylang.parser.LALRSimpleIdentifierParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Test
+	public void identifierTest() throws Exception {
+		String input = "id superman";
+		Assert.assertEquals("superman", 	parseIdentifier(input).getIdentifier());
+	}
+
+	private SimpleIdentifier parseIdentifier(String input) throws Exception {
+		LALRSimpleIdentifierParser parser = new LALRSimpleIdentifierParser();
+		return parser.parse(input);
+	}
+}
